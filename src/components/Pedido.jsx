d64312ade56ced5b18cfecee13213f0ab471e1e4
Lucas <labsfiap@fiap.com.br>
2026-09-18 08:44:51 -0300

    feat: bancos de dados do projeto

---
+++ src/components/Pedido.jsx
@@ -0,0 +1,18 @@
+import { UseState } from "react"
+
+const cardapio=[
+    {id:1,nome:"Combo-01",preco:25.00,disponivel:true,quantidade:0}
+    { id: 2, nome: "Combo-01", preco: 35.00, disponivel: true, quantidade: 0 }
+    { id: 3, nome: "Combo-01", preco: 45.00, disponivel: false, quantidade: 0 }
+    { id: 4, nome: "Combo-01", preco: 55.00, disponivel: true, quantidade: 0 }    
+]
+
+const Pedido = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Pedido
